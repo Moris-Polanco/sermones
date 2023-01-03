c595a05a8c21408776db9475e9e0c0430570a9b5
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-02 20:55:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+import openai
+import streamlit as st
+import os
 
+# Autenticación de OpenAI (oculta la clave en una variable de entorno)
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+def generate_article(title, outline):
+  model_engine = "text-davinci-003"
+  prompt = (f"Escribe un sermón  con ocasión de '{title}' basado en el siguiente esquema:\n{outline}. El sermón debe contener al menos una cita bíblica en cada sección")
+
+  completions = openai.Completion.create(
+      engine=model_engine,
+      prompt=prompt,
+      max_tokens=3024,
+      n=1,
+      stop=None,
+      temperature=0.7,
+  )
+
+  article = completions.choices[0].text
+  return article
+
+st.title("Generador de sermones")
+
+sidebar = st.sidebar
+title = sidebar.text_input("Título:")
+outline = sidebar.text_area("Esquema:")
+
+if st.button("Generar sermón"):
+  article = generate_article(title, outline)
+  st.success(article)
